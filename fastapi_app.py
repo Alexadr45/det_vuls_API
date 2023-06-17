b5df27e681fe02ea88f5bf9fe0288025b0c3902b
Anton Vasilev <113251534+Ch00cha@users.noreply.github.com>
2023-06-17 20:22:10 +0500

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -70,7 +70,7 @@ async def create_upload_file(file: UploadFile = File(...)):
     os.remove(file.filename)
     result = {f_name : res_preds}
     result = json.dumps(result)
-    return result
+    return result.json()
 
 
 if __name__ == "__main__":
