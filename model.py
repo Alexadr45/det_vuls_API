b0056a6a2dc8b94994b1ea9367e3f88fe8892e7e
Слава <113251598+yashka2210@users.noreply.github.com>
2023-06-18 21:22:13 +0500

    Update model.py

--- model.py
+++ model.py
@@ -248,10 +248,12 @@ def get_all_lines_score(word_scores: list):
     score_sum = 0
     line_idx = 0
     line = ""
-    for i in range(len(word_att_scores)):
+    for i in range(len(word_scores)):
         # summerize if meet line separator or the last token
-        if (word_scores[i][0] in separator or i == (len(word_scores) - 1)) \
-            and score_sum != 0:
+        if all([any([word_scores[i][0] in separator,
+                     (len(word_scores) - 1)]),
+                score_sum != 0]):
+        # if (word_scores[i][0] in separator or i == (len(word_scores) - 1)) and score_sum != 0:
             score_sum += word_scores[i][1]
             all_lines_score.append(score_sum)
             line = ""
