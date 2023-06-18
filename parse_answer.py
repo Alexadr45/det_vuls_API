5f52876a1797ff59e9db9bda746a7cb3363e64d9
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-06-18 23:50:32 +0500

    Update parse_answer.py

--- parse_answer.py
+++ parse_answer.py
@@ -7,7 +7,7 @@ def print_predict(file):
         data = json.load(f)
     with open('predict.txt', 'w') as f:
         for name in data.keys():
-            if data[filename] == 'Уязвимости не найдены':
+            if data[name] == 'Уязвимости не найдены':
                 f.write('Уязвимости не найдены')
             else:
                 f.writelines([f"Имя файла: {name}\n",
@@ -16,7 +16,7 @@ def print_predict(file):
                     f.writelines([f"Метод {i+1}:\n",
                                   "Уязвимые строки:\n",
                                   '\n'.join(map(str,
-                                       data[name][m]['vul_lines'])),
+                                                data[name][m]['vul_lines'])),
                                   "\n", 'Код:\n',
                                   ''.join(data[name][m]['orig_func'])])
 
